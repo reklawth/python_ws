963a10d2743e53d6c8fc17dda6f7c09a3e681f6b
Thomas Walker <thwalker@vt.edu>
2020-06-11 21:59:30 -0400

    Adds implicit chaining

---
+++ errors_exceptions/heron.py
@@ -0,0 +1,29 @@
+#! usr/bin/python3
+# UTF-8
+
+import math
+
+class TriangleError(Exception):
+
+    def __init__(self, text, sides):
+        super().__init__(text) # <- Message forwared to base class constructor
+        self._sides = tuple(sides) # <- side lengths are stored in an instance attribute in the derived class
+
+    @property
+    def sides(self):
+        return self._sides
+
+    def __str__(self):
+        return "'{}' for sides {}".format(self.args[0], self._sides)
+
+    def __repr__(self):
+        return "TriangleError({!r}, {!r}".format(self.args[0], self._sides)
+
+def triangle_area(a, b, c):
+    sides = sorted((a, b, c))
+    if sides[2] > sides[0] + sides[1]:
+        raise TriangleError("Illegal triangle", sides)
+    
+    p = (a + b + c) / 2
+    a = math.sqrt(p * (p - a) * (p - b) * (p -c))
+    return a
