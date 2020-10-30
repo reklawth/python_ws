aec3bc9a70fbcd27e05d1c28be859ef56c250e8f
Thomas Walker <thwalker@vt.edu>
2020-10-30 09:31:21 -0400

    Creating a request and parsing

---
+++ py_automation/exercise_files/my_code/scrape.py
@@ -0,0 +1,10 @@
+# %%
+# Creating a request and parsing
+import requests
+from bs4 import BeautifulSoup
+url = 'https://quotes.toscrape.com/'
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'lxml')
+print(soup)
+
+# %%
